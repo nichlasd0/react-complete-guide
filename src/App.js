991a3ba978970a639cd28ba143fcb3515c64254c
Nichlas <nichlasd@gmail.com>
2021-04-20 18:06:59 +0200

    section 3 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const expenses = [
+        {id: 'e1', title: 'Toilet paper', amount: 4.23, date: new Date(2021, 1, 3)},
+        {id: 'e2', title: 'New TV', amount: 1499.99, date: new Date(2021, 2, 16)},
+        {id: 'e3', title: 'Car insurance', amount: 295.23, date: new Date(2021, 4, 20)},
+        {id: 'e4', title: 'Food', amount: 85.23, date: new Date(2021, 4, 13)}
+    ]
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <Expenses items={expenses}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Expenses/expenseDate.js
@@ -0,0 +1,18 @@
+﻿import './expenseDate.css'
+
+function ExpenseDate(props) {
+
+    const month = props.date.toLocaleString('sv-SE', {month: 'long'});
+    const day = props.date.toLocaleString('sv-SE', {day: '2-digit'});
+    const year = props.date.getFullYear();
+    
+    return (
+        <div className="expense-date">
+            <div className="expense-date__year">{year}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__day">{day}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
